4e573fcf0a59abbabef52e1fb5824c88403e593a
BaldwinRellora23 <120635798+BaldwinRellora23@users.noreply.github.com>
2024-09-27 17:51:30 +0800

    Initial

---
+++ Test_Npm_BAckup/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.logMessage = void 0;
+const logMessage = (entity) => {
+    console.log("Name: " + entity.Name + " ,Age : " + entity.Age);
+};
+exports.logMessage = logMessage;
