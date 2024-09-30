6b20ecf0d4d6a9513e8c4b30f4f0f34c14f9a1d9
BaldwinRellora23 <baldwinnr@stpeter.com.ph>
2024-09-30 10:34:06 +0800

    test

--- index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logMessage = void 0;
-const logMessage = (entity) => {
-    console.log("Name: " + entity.Name + " ,Age : " + entity.Age);
-};
-exports.logMessage = logMessage;
